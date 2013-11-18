b42147cf7917acaee9ae5070ffcb5eeba09e1a81
Jesse Shawl <jesse@jshawl.com>
2013-11-17 21:08:05 -0500

    added syntax theming and type size changes

--- config.rb
+++ config.rb
@@ -1,38 +1,18 @@
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "/"
+debug = :true
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-output_style = :nested
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-#
 
 require 'autoprefixer-rails'
 require 'csso'
 
-
 on_stylesheet_saved do |file|
-      css = File.read(file)
-            File.open(file, 'w') do |io|
-            io << Csso.optimize( AutoprefixerRails.compile(css) )
-                             end
+  css = File.read(file)
+  File.open(file,'w') do |io|
+    if debug == :true
+      io << AutoprefixerRails.compile(css)
+    else
+      io << Csso.optimize(AutoprefixerRails.compile(css))
+    end
+  end
 end
+
